797858b8a123aa63f84b44cb1fc281235d534ba9
Ken Sonoda <ken.soda0@gmail.com>
2017-06-18 16:08:18 +0900

    Play noise
    Cache

---
+++ docs/scripts/main.js
@@ -0,0 +1,54 @@
+/* eslint-env browser */
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+            .register('/sw.js')
+            .then(function() { console.log('Service Worker Registered'); });
+}
+const logger = console.debug;
+const noise = {
+  init: function() {
+    this.context = new AudioContext();
+    this.connect(this.mkBrown());
+    this.suspend();
+    this.bind();
+  },
+  bind: function () {
+    document.querySelector('#play').onchange = this.updateNoise.bind(this);
+  },
+  connect: function(source) {
+    // const g = this.context.createGain();
+    // source.connect(g);
+    // g.connect(this.context.destination);
+    source.connect(this.context.destination);
+  },
+  mkBrown: function() {
+    const frameCount = Math.pow(2, 12);
+    const node = this.context.createScriptProcessor(frameCount, 1, 1);
+    node.onaudioprocess = onProcess;
+    return node;
+
+    function onProcess(e) {
+      const nowBuffering = e.outputBuffer.getChannelData(0);
+      let lastPower = 0.0;
+      for (let i = 0; i < frameCount; i++) {
+        const white = Math.random() * 2 - 1;
+        lastPower = (lastPower + (0.02 * white)) / 1.02; // -12dB/octave
+        nowBuffering[i] = 2 * lastPower; // compensate for gain
+      }
+    }
+  },
+  suspend: function() {
+    this.context.suspend()
+  },
+  resume: function() {
+    this.context.resume()
+  },
+  updateNoise: function (e){
+    logger(e.target.checked);
+    if(e.target.checked) this.resume();
+    else this.suspend();
+  }
+}
+
+window.onload = noise.init.bind(noise);
---
+++ docs/sw.js
@@ -0,0 +1,67 @@
+/*!
+ Copyright 2015 Google Inc. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+/* eslint-env worker */
+
+'use strict';
+
+// global.importScripts('https://cdnjs.cloudflare.com/ajax/libs/sw-toolbox/3.6.1/sw-toolbox.js')
+// Incrementing CACHE_VERSION will kick off the install event and force previously cached
+// resources to be cached again.
+const CACHE_VERSION = 1;
+var filesToCache = [
+  '/',
+  '/styles/material.indigo-pink.min.css',
+  '/scripts/material.min.js',
+  '/scripts/main.js'
+];
+const cacheName = 'noise_' + CACHE_VERSION;
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        console.log(response)
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+/*
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+*/
